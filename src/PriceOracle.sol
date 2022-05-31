6782621968987f3064cb6839aa91984183d0b004
Rogelio Morrell <molekilla@gmail.com>
2022-05-31 07:30:04 -0500

    feat(contracts): migrated to solidity v0.8.1 (#21)
    * feat(contracts): migrated to solidity v0.8.1, added Hitchens Order Statistics tree https://github.com/ethersphere/storage-incentives/issues/20
    
    * feat(contracts): fix solc versions
    
    * fix(contracts): version 0.8.1
    
    * Only use yarn for package management
    
    * fix: reinstall ethereumjs-abi
    
    * fix: dependency as devDependency
    
    Co-authored-by: Daniel A. Nagy <nagy.da@gmail.com>

--- src/PriceOracle.sol
+++ src/PriceOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity ^0.7.4;
+pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./PostageStamp.sol";
 
