a0c2db4f37c63aec518995c9ad6a2008902c7609
istae <14264581+istae@users.noreply.github.com>
2022-09-01 14:17:35 +0300

    mint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,8 @@ import 'solidity-coverage';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
 import { task } from "hardhat/config";
-import { ethers } from 'ethers';
-import { boolean } from 'hardhat/internal/core/params/argumentTypes';
+import { ethers, ContractTransaction, ContractReceipt } from 'ethers';
+import { boolean, int, string } from 'hardhat/internal/core/params/argumentTypes';
 
 require("@nomiclabs/hardhat-ethers");
 
@@ -13,19 +13,16 @@ const GOERLI_PRIVATE_KEY = "/";
 const PRIVATE_KEY = "/";
 task("copy", "A sample task with params")
   .addParam("owner", "")
-  .addParam("batchid", "")
-  .addParam("initialbalance", "")
+  .addParam("batchid", "", "", string)
+  .addParam("initialbalance", "", 0, int)
   .addParam("depth", "")
-  .addParam("bucketdepth", "", "16")
+  .addParam("bucketdepth", "", 16, int)
   .addParam("immutable", "", false, boolean)
-  .addParam("postagecontract", "")
-  .addParam("tokenaddress", "")
+  .addParam("postagecontract", "", "")
+  .addParam("tokenaddress", "", "")
   .setAction(async (taskArgs, { ethers }) => {
 
     const [deployer] = await ethers.getSigners();
-
-    console.log(taskArgs.immutable)
-    console.log(typeof taskArgs.immutable)
 
     const PostageStamp = await ethers.getContractFactory("PostageStamp");
     const contract = PostageStamp.attach(taskArgs.postagecontract);
@@ -35,8 +32,24 @@ task("copy", "A sample task with params")
 
     let transferAmount = taskArgs.initialbalance * 2 ** taskArgs.depth;
 
-    let presult = await token.connect(deployer).approve(contract.address, 2 * transferAmount);
-    console.log("allow trx hash", presult.hash);
+    let result: ContractTransaction;
+    let receipt: ContractReceipt;
+
+    // // result = await token.connect(deployer).mint(deployer.address, "1000000000000000000");
+    // result = await token.connect(deployer).balanceOf(deployer.address);
+    // // receipt = await result.wait();
+    // console.log("approve receipt", result);
+    // return
+
+    result = await token.connect(deployer).balanceOf(deployer.address);
+    // result = await token.connect(deployer).balanceOf(deployer.address);
+    // let receipt: ContractReceipt = await result.wait();
+    console.log("approve receipt", result);
+    return
+
+    result = await token.connect(deployer).approve(contract.address, 2 * transferAmount);
+    receipt = await result.wait();
+    console.log("approve receipt", receipt);
 
     let batchid = `${taskArgs.batchid}`;
     if (!batchid.startsWith("0x")) {
@@ -44,8 +57,8 @@ task("copy", "A sample task with params")
     }
 
     // Now you can call functions of the contract
-    let result: ethers.ContractTransaction = await contract.copyBatch(ethers.utils.getAddress(taskArgs.owner), taskArgs.initialbalance, taskArgs.depth, taskArgs.bucketdepth, batchid, taskArgs.immutable);
-    let receipt: ethers.ContractReceipt = await result.wait();
+    result = await contract.copyBatch(ethers.utils.getAddress(taskArgs.owner), taskArgs.initialbalance, taskArgs.depth, taskArgs.bucketdepth, batchid, taskArgs.immutable);
+    receipt = await result.wait();
     console.log(result)
     console.log(receipt)
   });
@@ -99,8 +112,8 @@ const config: HardhatUserConfig = {
     private: {
       url: 'http://geth-incentives-test.testnet.internal',
       accounts: [process.env.PRIVATE_KEY as string],
-      gas: 3_000_000,
-      gasPrice: 1_000_000,
+      gas: 30_000_000,
+      gasPrice: 10_000_000,
     }
   },
   paths: {
