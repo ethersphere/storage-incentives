16538c76190334590c39b75f38bff79a73cac008
sig <dan@1up.digital>
2022-09-09 09:03:27 +0100

    add initial staking test

--- test/Staking.test.ts
+++ test/Staking.test.ts
@@ -1,14 +1,28 @@
 import { expect } from './util/chai';
 import { ethers, deployments, getNamedAccounts, getUnnamedAccounts } from 'hardhat';
+import { Signer,Contract } from 'ethers';
+
+// Named accounts used by tests.
+let deployer: string;
+let others: string[];
+
+// Before the tests, set named accounts and read deployments.
+before(async function () {
+  const namedAccounts = await getNamedAccounts();
+  deployer = namedAccounts.deployer;
+  others = await getUnnamedAccounts();
+});
+
+let stakeRegistry:Contract;
 
 describe('Staking', function () {
   describe('when deploying contract', function () {
     beforeEach(async function () {
+      stakeRegistry = await ethers.getContract('StakeRegistry');
       await deployments.fixture();
     });
 
     it('should deploy StakeRegistry', async function () {
-      const stakeRegistry = await ethers.getContract('StakeRegistry');
       expect(stakeRegistry.address).to.be.properAddress;
     });
   });
