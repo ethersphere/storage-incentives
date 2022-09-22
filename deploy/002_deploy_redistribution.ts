bce3247861fcadc86f97bd8c7b5ca1695eef015a
sig <dan@1up.digital>
2022-09-22 09:27:25 +0100

    add vars

--- deploy/002_deploy_redistribution.ts
+++ deploy/002_deploy_redistribution.ts
@@ -3,12 +3,12 @@ import { DeployFunction } from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { deploy, get} = deployments;
   const { deployer } = await getNamedAccounts();
 
   await deploy('Redistribution', {
     from: deployer,
-    args: [],
+    args: [(await get('Staking')).address, (await get('PostageStamp')).address],
     log: true,
   });
 };
