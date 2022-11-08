6a05c7f90be259c44a01602819daf2dd2f651888
sig <dan@1up.digital>
2022-11-08 18:22:27 +0000

    improve price oracle comments

--- src/PriceOracle.sol
+++ src/PriceOracle.sol
@@ -14,17 +14,22 @@ contract PriceOracle is AccessControl {
      */
     event PriceUpdate(uint256 price);
 
+    // Role allowed to update price
     bytes32 public constant PRICE_UPDATER_ROLE = keccak256("PRICE_UPDATER");
 
+    // The current price is the atomic unit
     uint256 public currentPrice;
 
-    uint256 public constant minimumPrice = 2**10;
+    // The minimum price allowed
+    uint256 public constant minimumPrice = 1024;
 
+    // Constants used to modulate the price, see below usage
     uint256[] public increaseRate = [0, 1069, 1048, 1032, 1024, 1021, 1015, 1003, 980];
 
+    // When the contract is paused, price changes are not effective
     bool public isPaused = false;
 
-    // the address of the postageStamp contract
+    // The address of the linked postageStamp contract
     PostageStamp public postageStamp;
 
     constructor(address _postageStamp) {
@@ -33,30 +38,53 @@ contract PriceOracle is AccessControl {
     }
 
     /**
-     * @notice Update the price on the oracle.
-     * @dev Can only be called by the price updater role.
+     * @notice Manually set the price.
+     * @dev Can only be called by the admin role.
      * @param _price The new price.
      */
     function setPrice(uint256 _price) external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "caller is not the admin");
         currentPrice = _price;
+
+        //enforce minimum price
         if (currentPrice < minimumPrice) {
             currentPrice = minimumPrice;
         }
+
         postageStamp.setPrice(currentPrice);
         emit PriceUpdate(currentPrice);
     }
 
+    /**
+     * @notice Pause the contract.
+     * @dev Can only be called by the admin role.
+     */
     function pause() external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "caller is not the admin");
         isPaused = true;
     }
 
+    /**
+     * @notice Unpause the contract.
+     * @dev Can only be called by the admin role.
+     */
     function unPause() external {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "caller is not the admin");
         isPaused = false;
     }
 
+    /**
+     * @notice Automatically adjusts the price, called from the Redistribution contract
+     * @dev The ideal redundancy in Swarm is 4 nodes per neighbourhood. Each round, the
+     * Redistribution contract reports the current amount of nodes in the neighbourhood
+     * who have commited and revealed truthy reserve commitment hashes, this is called
+     * the redundancy signal. If the redundancy signal is 4, no action is taken. If the
+     * redundancy signal is greater than 4, a price decrease is applied in order to
+     * reduce the incentive to run a node. If the redundancy signal is greater than 4,
+     * a price increase is applied in order to increase the incentive to run a node.
+     * Can only be called by the price updater role, this should be set to be the depoloyed
+     * Redistribution contract's address. Rounds down to return an integer.
+     */
     function adjustPrice(uint256 redundancy) external {
         if(isPaused == false){
             require(hasRole(PRICE_UPDATER_ROLE, msg.sender), "caller is not a price updater");
@@ -64,16 +92,24 @@ contract PriceOracle is AccessControl {
             uint256 multiplier = minimumPrice;
             uint256 usedRedundancy = redundancy;
 
+            // redundancy may not be zero
             require(redundancy > 0, "unexpected zero");
 
+            // maximum extra redundancy is 4
             if (redundancy > 8) {
                 usedRedundancy = 8;
             }
 
+            // use the increaseRate array of constants to determine
+            // the rate at which the price will modulate - if usedRedundancy
+            // is the target value 4 there is no change, > 4 causes an increase
+            // and < 4 a decrease.
             uint256 ir = increaseRate[usedRedundancy];
 
+            // the multiplier is used to ensure whole number
             currentPrice = (ir * currentPrice) / multiplier;
 
+            //enforce minimum price
             if (currentPrice < minimumPrice) {
                 currentPrice = minimumPrice;
             }
