dc7a98dc47eb62c4bab3247fab56a6d89cce03f7
Marko <adriadrop@gmail.com>
2023-03-24 17:08:39 +0100

    export file for one contract working, todo for others and special cases for BZZ

--- deploy/005_deploy_data.ts
+++ deploy/005_deploy_data.ts
@@ -2,14 +2,73 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { networkConfig, developmentChains } from '../helper-hardhat-config';
 import verify from '../utils/verify';
+import * as fs from 'fs';
+
+interface DeployedContract {
+  abi: Array<unknown>;
+  bytecode: string;
+  address: string;
+  block: number;
+  url: string;
+}
+
+interface DeployedData {
+  chainId: number;
+  networkId: number;
+  contracts: {
+    postageStamp: DeployedContract;
+    redistribution: DeployedContract;
+    staking: DeployedContract;
+    priceOracle: DeployedContract;
+    bzzToken: DeployedContract;
+  };
+}
+
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, network } = hre;
-  const { deploy, execute, read, log } = deployments;
+  const { deploy, execute, get, read, log } = deployments;
 
   const { deployer, oracle, redistributor } = await getNamedAccounts();
 
+  var deployedData: DeployedData;
+
+  deployedData = {
+    chainId: networkConfig[network.name].chainID,
+    networkId: networkConfig[network.name].networkID,
+    contracts: {
+      bzzToken: {} as DeployedContract,
+      staking: {} as DeployedContract,
+      postageStamp: {} as DeployedContract,
+      priceOracle: {} as DeployedContract,
+      redistribution: {} as DeployedContract,
+    },
+  } as DeployedData;
+
+  async function writeResult(deployedData: DeployedData) {
+    let fileName = '';
+
+    if (fileName.length == 0 || !fs.existsSync(fileName)) {
+      fileName = network.name + '_deployed.json';
+    }
+
+    fs.writeFileSync(fileName, JSON.stringify(deployedData, null, '\t'));
+    return deployedData;
+  }
+
+
   // TODO This is placeholder for saving deployed data, model it per 1558.ts file
+  //const stakingArtifact = await deployments.getArtifact("StakeRegistry");
+  const stakingContract = await get('PriceOracle');
+  console.log(stakingContract.receipt!.blockNumber);
+
+  deployedData['contracts']['staking']['abi'] = stakingContract.abi;
+  deployedData['contracts']['staking']['bytecode'] = stakingContract.bytecode!;
+  deployedData['contracts']['staking']['address'] = stakingContract.address;
+  deployedData['contracts']['staking']['block'] = stakingContract.receipt!.blockNumber;
+  deployedData['contracts']['staking']['url'] = networkConfig[network.name].scanLink + stakingContract.address;
+
+  await writeResult(deployedData);
   log('----------------------------------------------------');
 };
 
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,8 +1,12 @@
+// Chain IDs are often the same but could be different https://chainid.network/chains_mini.json
+
 export interface networkConfigItem {
   bzzAddress?: string;
   deployedBlock?: number;
   blockConfirmations?: number;
   networkID?: number;
+  chainID?: number;
+  scanLink?: string;
 }
 
 export interface networkConfigInfo {
@@ -12,21 +16,29 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
   localhost: {
     networkID: 0,
+    chainID: 0,
+    scanLink: "https://goerli.etherscan.io/address/",
   },
   hardhat: {
     networkID: 0,
+    chainID: 0,
+    scanLink: "https://goerli.etherscan.io/address/",
   },
   testnet: {
     bzzAddress: '0x2aC3c1d3e24b45c6C310534Bc2Dd84B5ed576335',
     deployedBlock: 4224739,
     blockConfirmations: 6,
-    networkID: 5
+    networkID: 5,
+    chainID: 5,
+    scanLink: "https://goerli.etherscan.io/address/",
   },
   mainnet: {
     bzzAddress: '0xdBF3Ea6F5beE45c02255B2c26a16F300502F68da',
     deployedBlock: 16514506,
     blockConfirmations: 6,
-    networkID: 100
+    networkID: 100,
+    chainID: 100,
+    scanLink: "https://gnosisscan.io/address/",
   },
 };
 
