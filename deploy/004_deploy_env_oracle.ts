36cb56dd519d11754a971f627be4260a9c521c35
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-04-04 17:57:41 +0200

    feat: deploy script

---
+++ deploy/004_deploy_env_oracle.ts
@@ -0,0 +1,18 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ENV_ORACLE_BEE_VERSION } from '../scripts/utils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const args = [ENV_ORACLE_BEE_VERSION];
+
+  await deploy('EnvOracle', {
+    from: deployer,
+    args,
+    log: true,
+  });
+};
+
+export default func;
