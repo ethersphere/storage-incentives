b8b99122cdedf3acee47b5128d9000fe64509ea0
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-04-04 17:58:21 +0200

    test: env oracle

--- test/EnvOracle.test.ts
+++ test/EnvOracle.test.ts
@@ -1,16 +1,68 @@
 import { expect } from 'chai';
-import { ethers, deployments } from 'hardhat';
+import { Contract } from 'ethers';
+import { ethers, deployments, getUnnamedAccounts } from 'hardhat';
+import { ENV_ORACLE_BEE_VERSION } from '../scripts/utils';
 
-const TEST_BEE_VERSION = '1.13.0';
+const errors = {
+  setMinimumBeeVersion: 'Minimum Bee version should be in semver form',
+  permission: 'only administrator can use copy method',
+};
 
 describe('EnvOracle', function () {
+  let envOracle: Contract;
+  let otherAccounts: string[];
+
   beforeEach(async function () {
     await deployments.fixture();
+    envOracle = await ethers.getContract('EnvOracle');
+    otherAccounts = await getUnnamedAccounts();
   });
 
-  it('should deploy the contract with the right version', async () => {
-    const EnvOracle = await ethers.getContractFactory('EnvOracle');
-    const envOracle = await EnvOracle.deploy(TEST_BEE_VERSION);
-    expect(await envOracle.minimumBeeVersion()).to.equal(TEST_BEE_VERSION);
+  it('should match the deployed minimum Bee version with the default one', async () => {
+    expect(await envOracle.minimumBeeVersion()).to.equal(ENV_ORACLE_BEE_VERSION);
+  });
+
+  it('should change the version to different correct ones', async () => {
+    const setterChecker = async function (version: string) {
+      await envOracle.setMinimumBeeVersion(version);
+      expect(await envOracle.minimumBeeVersion()).to.equal(version);
+    };
+
+    await setterChecker('0.0.1');
+    await setterChecker('99.999.9999');
+  });
+
+  it('should not change the version in a wrong format', async () => {
+    const revertedChecker = async function (version: string) {
+      await expect(envOracle.setMinimumBeeVersion(version)).to.be.revertedWith(errors.setMinimumBeeVersion);
+    };
+
+    await revertedChecker('0.0.1.');
+    await revertedChecker('0.a.1');
+    await revertedChecker('0.1');
+    await revertedChecker('0.0.-1');
+    await revertedChecker('');
+  });
+
+  it('should not change minimum version without admin permission', async () => {
+    const revertedChecker = async function (account: string) {
+      const envOracle = await ethers.getContract('EnvOracle', account);
+      await expect(envOracle.setMinimumBeeVersion('0.0.1')).to.be.revertedWith(errors.permission);
+    };
+
+    for (const account of otherAccounts) {
+      await revertedChecker(account);
+    }
+  });
+
+  it('should grant admin role to other account to perform action', async () => {
+    const otherAccount = otherAccounts[0];
+    const envOracle2 = await ethers.getContract('EnvOracle', otherAccount);
+    await expect(envOracle2.setMinimumBeeVersion('0.0.1')).to.be.revertedWith(errors.permission);
+
+    const adminRole = await envOracle.DEFAULT_ADMIN_ROLE();
+    await envOracle.grantRole(adminRole, otherAccount);
+    await envOracle2.setMinimumBeeVersion('0.0.1');
+    expect(await envOracle2.minimumBeeVersion()).to.equal('0.0.1');
   });
 });
