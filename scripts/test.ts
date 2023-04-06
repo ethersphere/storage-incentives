613c636339b7b5be160ad2a8d674ba13717669c1
Marko <adriadrop@gmail.com>
2023-04-06 18:15:16 +0200

    fixes

--- scripts/test.ts
+++ scripts/test.ts
@@ -101,39 +101,20 @@ const config: ChainConfig = configs[network.name]
 async function main() {
   // This is deployer script for emergency deployment of only the redistribution contract with some quick fixes
   let args: string[] = [];
-  if (network.name == 'mainnet') {
-    // Staking, Stamps, Oracle args
-    args = [
-      '0x781c6D1f0eaE6F1Da1F604c6cDCcdB8B76428ba7',
-      '0x30d155478eF27Ab32A1D578BE7b84BC5988aF381',
-      '0x344A2CC7304B32A87EfDC5407cD4bEC7cf98F035',
-    ];
-  } else if (network.name == 'testnet') {
-    args = [
-      '0xCb07bf0603da228C8ec602bf12b973b8A94f9bac',
-      '0x1f87FEDa43e6ABFe1058E96A07d0ea182e7dc9BD',
-      '0x3e475aEAB162E28fee46E69225af446D3c4f3Bd3',
-    ];
-  }
+  args = [
+    '0xCb07bf0603da228C8ec602bf12b973b8A94f9bac',
+    '0x1f87FEDa43e6ABFe1058E96A07d0ea182e7dc9BD',
+    '0x3e475aEAB162E28fee46E69225af446D3c4f3Bd3',
+  ];
 
   // Deploy the contract
   const redisFactory = await ethers.getContractFactory('Redistribution');
   console.log('Deploying contract...');
   const redis = await redisFactory.deploy(...args);
-  await redis.deployed();
+  const tx1 = await redis.deployed();
   console.log(`Deployed contract to: ${redis.address}`);
-  await redis.deployTransaction.wait(6);
+  //await redis.deployTransaction.wait(6);
 
-  // Change roles on current stamps contract
-  const postageStampContract = await ethers.getContractAt('PostageStamp', args[1]);
-  const redistributorRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('REDISTRIBUTOR_ROLE'));
-  const tx = await postageStampContract.grantRole(redistributorRole, redis.address);
-  console.log('Changed REDISTRIBUTOR ROLE at : ', tx.hash);
-
-  // Change roles on current staking contract
-  const stakingContract = await ethers.getContractAt('StakeRegistry', args[0]);
-  const tx2 = await stakingContract.grantRole(redistributorRole, redis.address);
-  console.log('Changed REDISTRIBUTOR ROLE at : ', tx2.hash);
 
   // Add metadata for Bee Node
   const deployed = await JSON.parse(JSON.stringify(config.deployedData).toString());
@@ -145,11 +126,8 @@ async function main() {
   deployed['contracts']['redistribution']['url'] = config.url;
 
   fs.writeFileSync(config.networkName + '_deployed.json', JSON.stringify(deployed, null, '\t'));
+  console.log(config);
 
-  if (process.env.MAINNET_ETHERSCAN_KEY || process.env.TESTNET_ETHERSCAN_KEY) {
-    console.log('Verifying...');
-    await verify(redis.address, args);
-  }
 }
 
 main()
