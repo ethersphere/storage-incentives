735d7e8f33a3b8eeeaf74fbc609b4b7982dd5a4f
Marko <adriadrop@gmail.com>
2023-04-06 18:04:35 +0200

    add code for creating metadata for bee nodes

--- scripts/deploy_redis.ts
+++ scripts/deploy_redis.ts
@@ -1,9 +1,101 @@
 import 'hardhat-deploy-ethers';
 import '@nomiclabs/hardhat-etherscan';
-import { ethers, network, hre } from 'hardhat';
+import { ethers, network } from 'hardhat';
 import verify from '../utils/verify';
+import hre from 'hardhat';
+import * as fs from 'fs';
 
+interface DeployedContract {
+  abi: Array<unknown>;
+  bytecode: string;
+  address: string;
+  block: number;
+  url: string;
+}
 
+interface DeployedData {
+  exists: boolean;
+  chainId: number;
+  networkId: number;
+  contracts: {
+    bzzToken: DeployedContract;
+    staking: DeployedContract;
+    postageStamp: DeployedContract;
+    priceOracle: DeployedContract;
+    redistribution: DeployedContract;
+  };
+}
+
+interface ContractData {
+  addresses: {
+    bzzToken: string;
+    staking: string;
+    postageStamp: string;
+    priceOracle: string;
+    redistribution: string;
+  };
+  blocks: {
+    bzzToken: number;
+    staking: number;
+    postageStamp: number;
+    priceOracle: number;
+    redistribution: number;
+  };
+}
+
+interface ChainConfig {
+  chainId?: number;
+  networkId?: number;
+  networkName: string;
+  deployedData: DeployedData;
+  url: string;
+}
+
+let networkDeployedData: DeployedData;
+try {
+  networkDeployedData = require('../' + network.name + '_deployed.json');
+  networkDeployedData.exists = true;
+} catch (e) {
+  networkDeployedData = {
+    exists: false,
+    chainId: 0,
+    networkId: 0,
+    contracts: {
+      bzzToken: {} as DeployedContract,
+      staking: {} as DeployedContract,
+      postageStamp: {} as DeployedContract,
+      priceOracle: {} as DeployedContract,
+      redistribution: {} as DeployedContract,
+    },
+  } as DeployedData;
+}
+
+const configs: Record<string, ChainConfig> = {
+  testnet: {
+    chainId: network.config.chainId,
+    networkId: networkDeployedData.networkId ? networkDeployedData.networkId : 10,
+    networkName: network.name,
+    deployedData: networkDeployedData,
+    url: hre.config.etherscan.customChains[0]['urls']['browserURL'].toString(),
+  },
+  mainnet: {
+    chainId: network.config.chainId,
+    networkId: networkDeployedData.networkId ? networkDeployedData.networkId : 100,
+    networkName: network.name,
+    deployedData: networkDeployedData,
+    url: hre.config.etherscan.customChains[1]['urls']['browserURL'].toString(),
+  },
+};
+
+const config: ChainConfig = configs[network.name]
+  ? configs[network.name]
+  : ({
+    chainId: network.config.chainId,
+    networkId: networkDeployedData.networkId ? networkDeployedData.networkId : network.config.chainId,
+    networkName: network.name,
+    deployedData: networkDeployedData,
+    url: '',
+  } as ChainConfig);
 
 
 async function main() {
@@ -44,9 +136,15 @@ async function main() {
   console.log('Changed REDISTRIBUTOR ROLE at : ', tx2.hash);
 
   // Add metadata for Bee Node
+  const deployed = await JSON.parse(JSON.stringify(config.deployedData).toString());
+  const redisABI = await require('../artifacts/src/Redistribution.sol/Redistribution.json');
+  deployed['contracts']['redistribution']['abi'] = redisABI.abi;
+  deployed['contracts']['redistribution']['bytecode'] = redisABI.bytecode.toString();
+  deployed['contracts']['redistribution']['address'] = redis.address;
+  deployed['contracts']['redistribution']['block'] = parseInt(redis.blockNumber, 16);
+  deployed['contracts']['redistribution']['url'] = config.url;
 
-  //networkDeployedData = require('../' + network.name + '_deployed.json');
-  //const deployed = await JSON.parse(JSON.stringify(networkDeployedData).toString());
+  fs.writeFileSync(config.networkName + '_deployed.json', JSON.stringify(deployed, null, '\t'));
 
   if (process.env.MAINNET_ETHERSCAN_KEY || process.env.TESTNET_ETHERSCAN_KEY) {
     console.log('Verifying...');
