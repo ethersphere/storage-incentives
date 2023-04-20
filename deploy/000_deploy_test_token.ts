8800d0226a19ae64a5700d4dbc9ae27662d900c4
Marko <adriadrop@gmail.com>
2023-04-20 16:30:14 +0200

    add test token as zero step

---
+++ deploy/000_deploy_test_token.ts
@@ -0,0 +1,39 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { developmentChains } from '../helper-hardhat-config';
+import verify from '../utils/verify';
+
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  let token = null;
+
+  if (developmentChains.includes(network.name)) {
+    const argsToken = ['TEST', 'TST', '1249989122910552325012092'];
+
+    token = await deploy('TestToken', {
+      from: deployer,
+      args: argsToken,
+      log: true,
+    });
+  }
+
+  if (network.name == 'testnet' || network.name == 'sepolia') {
+    const argsToken = ['gBZZ', 'gBZZ', '1250000000000000000000000'];
+    token = await deploy('TestToken', {
+      from: deployer,
+      args: argsToken,
+      log: true,
+    });
+
+    if (process.env.TESTNET_ETHERSCAN_KEY) {
+      console.log('Verifying...');
+      await verify(token.address, argsToken);
+    }
+  }
+  log('----------------------------------------------------');
+
+};
+
+export default func;
+func.tags = ['main', 'testToken', 'contracts'];
---
+++ deploy/001_deploy_postage.ts
@@ -0,0 +1,20 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { networkConfig } from '../helper-hardhat-config';
+
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
+  const { deploy, log, get } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const argsStamp = [(await get('TestToken')).address, 16];
+
+  await deploy('PostageStamp', {
+    from: deployer,
+    args: argsStamp,
+    log: true,
+    waitConfirmations: networkConfig[network.name]?.blockConfirmations || 1,
+  });
+
+  log('----------------------------------------------------');
+};
+
+export default func;
+func.tags = ['main', 'postageStamp', 'contracts'];
