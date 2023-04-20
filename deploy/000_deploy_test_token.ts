963d127aea5865546e946f012c5bbcb24b667512
Marko <adriadrop@gmail.com>
2023-04-20 16:33:38 +0200

    fixes

--- deploy/000_deploy_test_token.ts
+++ deploy/000_deploy_test_token.ts
@@ -1,5 +1,5 @@
 import { DeployFunction } from 'hardhat-deploy/types';
-import { developmentChains } from '../helper-hardhat-config';
+import { developmentChains, networkConfig } from '../helper-hardhat-config';
 import verify from '../utils/verify';
 
 const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
@@ -24,6 +24,7 @@ const func: DeployFunction = async function ({ deployments, getNamedAccounts, ne
       from: deployer,
       args: argsToken,
       log: true,
+      waitConfirmations: networkConfig[network.name]?.blockConfirmations || 1,
     });
 
     if (process.env.TESTNET_ETHERSCAN_KEY) {
@@ -31,8 +32,8 @@ const func: DeployFunction = async function ({ deployments, getNamedAccounts, ne
       await verify(token.address, argsToken);
     }
   }
-  log('----------------------------------------------------');
 
+  log('----------------------------------------------------');
 };
 
 export default func;
--- scripts/deploy_gBZZ.ts
+++ scripts/deploy_gBZZ.ts
@@ -4,15 +4,14 @@ import verify from '../utils/verify';
 import { developmentChains } from '../helper-hardhat-config';
 
 async function main() {
-
   if (network.name == 'testnet' || network.name == 'sepolia') {
     const args = ['gBZZ', 'gBZZ', '1250000000000000000000000'];
-    const gBzzTokenFactory = await ethers.getContractFactory("TestToken");
-    console.log("Deploying contract...");
+    const gBzzTokenFactory = await ethers.getContractFactory('TestToken');
+    console.log('Deploying contract...');
     const gBzzToken = await gBzzTokenFactory.deploy(...args);
-    await gBzzToken.deployed()
+    await gBzzToken.deployed();
     console.log(`Deployed contract to: ${gBzzToken.address}`);
-    await gBzzToken.deployTransaction.wait(6)
+    await gBzzToken.deployTransaction.wait(6);
 
     if (!developmentChains.includes(network.name) && process.env.TESTNET_ETHERSCAN_KEY) {
       console.log('Verifying...');
