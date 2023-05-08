62d3352e2db691eb088c9cc1b4594588ef783f5a
Marko <adriadrop@gmail.com>
2023-05-08 16:36:56 +0200

    remove obs

--- deploy/001_deploy_postage.ts
+++ deploy/001_deploy_postage.ts
@@ -1,7 +1,7 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 import { networkConfig } from '../helper-hardhat-config';
 
-const func: DeployFunction = async function ({ deployments, getNamedAccounts, network, upgrades }) {
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
   const { deploy, log, get } = deployments;
   const { deployer } = await getNamedAccounts();
   const argsStamp = [(await get('TestToken')).address, 16];
