ef29f07ced61b7ea04fc409dbb56db3df4449192
Marko <adriadrop@gmail.com>
2023-05-11 18:06:33 +0200

    comment

--- deploy/001_deploy_postage.ts
+++ deploy/001_deploy_postage.ts
@@ -6,9 +6,9 @@ const func: DeployFunction = async function ({ deployments, getNamedAccounts, ne
   const { deployer } = await getNamedAccounts();
   const argsStamp = [(await get('TestToken')).address, 16];
 
-  // This code will deploy UUPS proxy with 0x70058cC8A9e538140007853fE7c553eBE1773C06 contract on first run, after that it will reuse proxy and stamp if they are the same as before
-  // if PostageStamp contract has any new change in it, it will recompile, redeploy as implementation to original proxy, just need to keep saveDeployments option
-  // as default which is true for all networks other then hardhat network
+  // This code will deploy UUPS proxy contract on first run, after that it will reuse proxy
+  // If PostageStamp contract has any changes,hardhat will recompile it and redeploy as implementation to original proxy
+  // What to watch out when using UUPS is listed here https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#modifying-your-contracts.adoc
 
   await deploy('PostageStamp', {
     from: deployer,
