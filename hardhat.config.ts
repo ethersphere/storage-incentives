aed1f18efe2137188d23a64a9df4817b044e4b11
Viktor Tóth @nugaon <toth.viktor.levente@gmail.com>
2023-07-07 09:23:24 +0000

    test: mined witnesses for stats

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,6 +33,9 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  mocha: {
+    timeout: Number.MAX_SAFE_INTEGER,
+  },
   preprocess: {
     eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'),
   },
