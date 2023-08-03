3499582af12f9747ac2b1fbd1484c5c646d5c3b6
Mark Bliss <adriadrop@gmail.com>
2023-08-03 13:04:35 +0200

    feat: add gas reports (#170)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import 'hardhat-deploy-ethers';
 import 'hardhat-tracer';
 import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-contract-sizer';
+import 'hardhat-gas-reporter';
 import { removeConsoleLog } from 'hardhat-preprocessor';
 
 // Set Private RPCs if added, otherwise use Public that are hardcoded in this config
@@ -166,6 +167,17 @@ const config: HardhatUserConfig = {
   },
   contractSizer: {
     runOnCompile: true,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
+    token: 'ETH',
+    onlyCalledMethods: true,
+    //outputFile: 'gas-report.txt',
+    //noColors: true,
+    //gasPrice: 8,
+    coinmarketcap: process.env.CMC_KEY,
   },
 };
 
