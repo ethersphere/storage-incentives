ddb329eca9099cbaa18ffe5e3aff249633d7cb4d
marko.b <adriadrop@gmail.com>
2023-09-25 21:27:39 +0200

    make script for oracle single deployment

--- scripts/deploy_oracle.ts
+++ scripts/deploy_oracle.ts
@@ -21,7 +21,7 @@ interface DeployedData {
     staking: DeployedContract;
     postageStamp: DeployedContract;
     priceOracle: DeployedContract;
-    redistribution: DeployedContract;
+    oracle: DeployedContract;
   };
 }
 
@@ -45,7 +45,7 @@ try {
       staking: {} as DeployedContract,
       postageStamp: {} as DeployedContract,
       priceOracle: {} as DeployedContract,
-      redistribution: {} as DeployedContract,
+      oracle: {} as DeployedContract,
     },
   } as DeployedData;
 }
@@ -78,67 +78,41 @@ const config: ChainConfig = configs[network.name]
     } as ChainConfig);
 
 async function main() {
-  // This is deployer script for emergency deployment of only the redistribution contract with some quick fixes
+  // This is deployer script for emergency deployment of only the oracle contract with some quick fixes
   let args: string[] = [];
   let waitTime = 6;
   if (network.name == 'mainnet') {
-    // Staking, Stamps, Oracle args, multisig
-    args = [
-      '0x781c6D1f0eaE6F1Da1F604c6cDCcdB8B76428ba7',
-      '0x30d155478eF27Ab32A1D578BE7b84BC5988aF381',
-      '0x344A2CC7304B32A87EfDC5407cD4bEC7cf98F035',
-      '0xb1C7F17Ed88189Abf269Bf68A3B2Ed83C5276aAe',
-    ];
+    //Stamps, Multisig
+    args = ['0x30d155478eF27Ab32A1D578BE7b84BC5988aF381', '0xb1C7F17Ed88189Abf269Bf68A3B2Ed83C5276aAe'];
   } else if (network.name == 'testnet') {
-    args = [
-      '0xCb07bf0603da228C8ec602bf12b973b8A94f9bac',
-      '0x1f87FEDa43e6ABFe1058E96A07d0ea182e7dc9BD',
-      '0x3e475aEAB162E28fee46E69225af446D3c4f3Bd3',
-      '0xb1C7F17Ed88189Abf269Bf68A3B2Ed83C5276aAe',
-    ];
+    args = ['0x1f87FEDa43e6ABFe1058E96A07d0ea182e7dc9BD', '0xb1C7F17Ed88189Abf269Bf68A3B2Ed83C5276aAe'];
   } else if (network.name == 'localhost') {
-    args = [
-      '0xFC402f08a61Ebfc4e30e1DB6Cfb38b79578709C1',
-      '0x9A2F29598CB0787Aa806Bbfb65B82A9e558945E7',
-      '0xF52458e65b8e3B69d93DD3803d8ef934c75E0022',
-      '0x3c8F39EE625fCF97cB6ee22bCe25BE1F1E5A5dE8',
-    ];
+    args = ['0x9A2F29598CB0787Aa806Bbfb65B82A9e558945E7', '0x3c8F39EE625fCF97cB6ee22bCe25BE1F1E5A5dE8'];
     waitTime = 1;
   }
 
   // Deploy the contract
-  const redisFactory = await ethers.getContractFactory('Redistribution');
+  const oracleFactory = await ethers.getContractFactory('PriceOracle');
   console.log('Deploying contract...');
-  const redis = await redisFactory.deploy(...args);
-  await redis.deployed();
-  console.log(`Deployed contract to: ${redis.address}`);
-  const deploymentReceipt = await redis.deployTransaction.wait(waitTime);
-
-  // Change roles on current stamps contract
-  const postageStampContract = await ethers.getContractAt('PostageStamp', args[1]);
-  const redistributorRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('REDISTRIBUTOR_ROLE'));
-  const tx = await postageStampContract.grantRole(redistributorRole, redis.address);
-  console.log('Changed REDISTRIBUTOR ROLE at : ', tx.hash);
-
-  // Change roles on current staking contract
-  const stakingContract = await ethers.getContractAt('StakeRegistry', args[0]);
-  const tx2 = await stakingContract.grantRole(redistributorRole, redis.address);
-  console.log('Changed REDISTRIBUTOR ROLE at : ', tx2.hash);
+  const oracle = await oracleFactory.deploy(...args);
+  await oracle.deployed();
+  console.log(`Deployed contract to: ${oracle.address}`);
+  const deploymentReceipt = await oracle.deployTransaction.wait(waitTime);
 
   // Add metadata for Bee Node
   const deployed = await JSON.parse(JSON.stringify(config.deployedData).toString());
-  const redisABI = await require('../artifacts/src/Redistribution.sol/Redistribution.json');
-  deployed['contracts']['redistribution']['abi'] = redisABI.abi;
-  deployed['contracts']['redistribution']['bytecode'] = redisABI.bytecode.toString();
-  deployed['contracts']['redistribution']['address'] = redis.address;
-  deployed['contracts']['redistribution']['block'] = deploymentReceipt.blockNumber;
-  deployed['contracts']['redistribution']['url'] = config.url + redis.address;
+  const oracleABI = await require('../artifacts/src/PriceOracle.sol/PriceOracle.json');
+  deployed['contracts']['priceOracle']['abi'] = oracleABI.abi;
+  deployed['contracts']['priceOracle']['bytecode'] = oracleABI.bytecode.toString();
+  deployed['contracts']['priceOracle']['address'] = oracle.address;
+  deployed['contracts']['priceOracle']['block'] = deploymentReceipt.blockNumber;
+  deployed['contracts']['priceOracle']['url'] = config.url + oracle.address;
 
   fs.writeFileSync(config.networkName + '_deployed.json', JSON.stringify(deployed, null, '\t'));
 
   if ((process.env.MAINNET_ETHERSCAN_KEY || process.env.TESTNET_ETHERSCAN_KEY) && network.name != 'localhost') {
     console.log('Verifying...');
-    await verify(redis.address, args);
+    await verify(oracle.address, args);
   }
 }
 
