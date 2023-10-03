9f309b93561ba07afada4dd56f3394720809092e
Mark Bliss <adriadrop@gmail.com>
2023-10-03 19:37:42 +0200

    chore: New release with stamp rc6 (#202)

--- deploy/006_deploy_roles_redistribution.ts
+++ deploy/006_deploy_roles_redistribution.ts
@@ -8,15 +8,6 @@ const func: DeployFunction = async function ({ deployments, getNamedAccounts }) 
 
   const redisAddress = (await get('Redistribution')).address;
 
-  const redistributorRoleStakeRegistry = await read('StakeRegistry', 'REDISTRIBUTOR_ROLE');
-  await execute('StakeRegistry', { from: deployer }, 'grantRole', redistributorRoleStakeRegistry, redisAddress);
-
-  const redistributorRolePostageStamp = await read('PostageStamp', 'REDISTRIBUTOR_ROLE');
-  await execute('PostageStamp', { from: deployer }, 'grantRole', redistributorRolePostageStamp, redisAddress);
-
-  const priceUpdaterRoleOracle = await read('PriceOracle', 'PRICE_UPDATER_ROLE');
-  await execute('PriceOracle', { from: deployer }, 'grantRole', priceUpdaterRoleOracle, redisAddress);
-
   log('----------------------------------------------------');
 };
 
