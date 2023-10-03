54900dd453dc35aa463ceaaf183bc565eb556c6e
marko.b <adriadrop@gmail.com>
2023-10-03 19:34:09 +0200

    deploy new redis and deploy proper roles

--- scripts/deploy_roles.ts
+++ scripts/deploy_roles.ts
@@ -4,11 +4,11 @@ import { ethers, network } from 'hardhat';
 import hre from 'hardhat';
 
 async function main() {
-  // Order of contracts and how should they be deployed
+  // Order of contracts and how should they be deployed, testnet addresses
   const currentPostage = '0xF5147D56502C80004f91FB4112d6812CddE8eDE3';
   const currentOracle = '0xd41A47fCaa67945A11a398F00D5f7F130aF03733';
-  const currentStaking = '0xF5147D56502C80004f91FB4112d6812CddE8eDE3';
-  const currentRedis = '0xF5147D56502C80004f91FB4112d6812CddE8eDE3';
+  const currentStaking = '0xCb07bf0603da228C8ec602bf12b973b8A94f9bac'; // old staking
+  const currentRedis = '0x264079eeF0CE42D790e3FA7DF8D0cfA675ef6504';
 
   // Change roles on current stamp contract
   const stamp = await ethers.getContractAt('PostageStamp', currentPostage);
