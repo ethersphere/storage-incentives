cb1a5021a037f3d08e54f9ae0901c73a6df4313c
marko.b <adriadrop@gmail.com>
2023-10-14 22:04:53 +0200

    remove comments

--- tasks/signatures.ts
+++ tasks/signatures.ts
@@ -2,17 +2,15 @@ import { task } from 'hardhat/config';
 
 task('sigs', 'Generate ABI signatures for errors and functions')
   .addParam('c', 'Name of the contract for which to generate ABI data')
-  .setAction(async (taskArgs: any, hre) => {
+  .setAction(async (taskArgs: any, { ethers }) => {
     // Load the contract ABI based on the contract name
     const ABI = require(`../artifacts/src/${taskArgs.c}.sol/${taskArgs.c}.json`).abi;
 
-    // Explicitly annotate the types of parameters 'e' and 'f'
     const prepareData = (e: { name: string; inputs: { type: string }[] }) =>
       `${e.name}(${e.inputs.map((param) => param.type)})`;
+    const encodeSelector = (f: string) => ethers.utils.id(f).slice(0, 10);
 
-    const encodeSelector = (f: string) => hre.ethers.utils.id(f).slice(0, 10);
-
-    // Parse ABI and encode its functions
+    // Parse ABI
     const output = ABI.filter((e: any) => ['function', 'error'].includes(e.type)).flatMap(
       (e: any) => `${encodeSelector(prepareData(e))}: ${prepareData(e)}`
     );
