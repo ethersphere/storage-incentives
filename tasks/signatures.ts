60aa0abffb2f2a9a98d2b0408207eb3426b075fa
marko.b <adriadrop@gmail.com>
2023-10-15 20:32:24 +0200

    add filename option

--- tasks/signatures.ts
+++ tasks/signatures.ts
@@ -2,9 +2,13 @@ import { task } from 'hardhat/config';
 
 task('sigs', 'Generate ABI signatures for errors and functions')
   .addParam('c', 'Name of the contract for which to generate ABI data')
+  .addOptionalParam('f', 'Name of Solidity file where contract is stored')
   .setAction(async (taskArgs: any, { ethers }) => {
+    // If 'f' is not defined, use the value of 'c' for it
+    const fileName = taskArgs.f || taskArgs.c;
+
     // Load the contract ABI based on the contract name
-    const ABI = (await import(`../artifacts/src/${taskArgs.c}.sol/${taskArgs.c}.json`)).abi;
+    const ABI = (await import(`../artifacts/src/${fileName}.sol/${taskArgs.c}.json`)).abi;
 
     const prepareData = (e: { name: string; inputs: { type: string }[] }) =>
       `${e.name}(${e.inputs.map((param) => param.type)})`;
