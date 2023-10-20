d61e1771cc915be1d25501182323938173013c5d
Marko <adriadrop@gmail.com>
2023-10-20 17:57:54 +0200

    add and remove roles

--- deploy/main/012_deploy_multisig.ts
+++ deploy/main/012_deploy_multisig.ts
@@ -1,16 +1,29 @@
 import { DeployFunction } from 'hardhat-deploy/types';
+import { networkConfig } from '../../helper-hardhat-config';
 
 // Used for setting states in contracts according to the currently working environment
-const func: DeployFunction = async function ({ deployments, getNamedAccounts }) {
-  const { execute, log } = deployments;
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, network }) {
+  const { execute, log, read } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  log('Setting Redistribution state "sampleMaxValue"');
+  if (network.name == 'mainnet') {
+    log('Deploy multisig to all contracts, remove deployer');
 
-  const sampleMaxValue = '3500000000000000000000000000000000000000000000000000000000000000000000000';
-  await execute('Redistribution', { from: deployer }, 'setSampleMaxValue', sampleMaxValue);
+    // ADD Roles to Multisig
+    const adminRole = await read('PostageStamp', 'DEFAULT_ADMIN_ROLE');
+    await execute('PostageStamp', { from: deployer }, 'grantRole', adminRole, networkConfig['mainnet'].multisig);
+    await execute('PriceOracle', { from: deployer }, 'grantRole', adminRole, networkConfig['mainnet'].multisig);
+    await execute('StakeRegistry', { from: deployer }, 'grantRole', adminRole, networkConfig['mainnet'].multisig);
+    await execute('Redistribution', { from: deployer }, 'grantRole', adminRole, networkConfig['mainnet'].multisig);
 
-  log('----------------------------------------------------');
+    // REMOVE Roles from deployer
+    await execute('PostageStamp', { from: deployer }, 'renounceRole', adminRole, deployer);
+    await execute('PriceOracle', { from: deployer }, 'renounceRole', adminRole, deployer);
+    await execute('StakeRegistry', { from: deployer }, 'renounceRole', adminRole, deployer);
+    await execute('Redistribution', { from: deployer }, 'renounceRole', adminRole, deployer);
+
+    log('----------------------------------------------------');
+  }
 };
 
 export default func;
