89a4480b5c07e30f90786e675478849310f3abde
Mark.B <adriadrop@gmail.com>
2023-10-24 19:08:10 +0200

    do it on main fork

--- scripts/stamp_migration.sh
+++ scripts/stamp_migration.sh
@@ -2,7 +2,7 @@
 # Using task defined in hardhat config, set PostageStamp contract that will be used in that config
 
 
-for row in $(cat ./private_testnet_batches.json | jq -c '.batches[]'); do
+for row in $(cat ./batches.json | jq -c '.batches[]'); do
     _field() {
         echo ${1} | jq -r ${2}
     }
@@ -16,7 +16,7 @@ for row in $(cat ./private_testnet_batches.json | jq -c '.batches[]'); do
     echo "current balance #####"
     echo ${balance}
 
-    cmd="npx hardhat --network testnet copy --owner ${owner} --initialbalance ${balance}  --depth ${depth} --bucketDepth ${bucketDepth}  --batchid ${batchid}  --immutable ${immutable}"
+    cmd="npx hardhat --network mainfork copy --owner ${owner} --initialbalance ${balance}  --depth ${depth} --bucketDepth ${bucketDepth}  --batchid ${batchid}  --immutable ${immutable}"
     $cmd
     [ $? -eq 0 ] && echo "${batchid} migration successful" || echo "${batchid} migration failure"
 done
