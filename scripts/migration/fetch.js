ba980b4656964f64253a83108c4cd33ac13cdba6
Mark.B <adriadrop@gmail.com>
2023-11-29 18:49:40 +0100

    add fetch and bump up newer ethers

---
+++ scripts/migration/fetch.js
@@ -0,0 +1,116 @@
+// This is SCRIPT for MIGRATION of Stamps, it fetches created contracts from BatchCreated EVENT
+
+const hre = require("hardhat");
+const fs = require("fs/promises");
+const { ethers } = require("hardhat");
+
+// Function to decode transaction input data
+async function decodeTransactionInput(transactionHash, provider) {
+  try {
+    // Get the transaction
+    const transaction = await provider.getTransaction(transactionHash);
+    if (!transaction) {
+      console.log("Transaction not found!");
+      return null;
+    }
+
+    const contractABI =
+      await require("../../artifacts/src/PostageStamp.sol/PostageStamp.json");
+
+
+    // Create an interface from the ABI to decode the data
+    const contractInterface = ethers.utils.Interface(contractABI.abi);
+
+    // Decode the transaction input data
+    const decodedInput = contractInterface.parseTransaction({
+      data: transaction.data,
+    });
+
+    // Convert decoded data to a more readable format
+    // Return only the args
+    return decodedInput.args;
+  } catch (error) {
+    console.error("Error decoding transaction input:", error);
+    return null;
+  }
+}
+
+// Custom replacer function to handle BigInt serialization
+function replacer(key, value) {
+  if (typeof value === "bigint") {
+    return value.toString();
+  }
+  return value;
+}
+
+async function main() {
+  const provider = ethers.provider;
+
+  // Specify the starting block
+  const startBlock = 30193660; // Replace with the block number from where you want to start
+
+  const contractAddress = "0x30d155478eF27Ab32A1D578BE7b84BC5988aF381";
+  const myContract = await ethers.getContractAt(
+    "PostageStamp",
+    contractAddress
+  );
+
+  // Prepare to store the decoded data
+  let decodedDataArray = [];
+
+  // Define batch size and delay (in milliseconds)
+  const batchSize = 10000; // Adjust based on your needs
+  const delay = 20; // Delay of 0.02 seconds
+
+  // Get the latest block number
+  const latestBlock = await provider.getBlockNumber();
+
+  for (
+    let currentBlock = startBlock;
+    currentBlock < latestBlock;
+    currentBlock += batchSize
+  ) {
+    // Calculate the end block for the current batch
+    const endBlock = Math.min(currentBlock + batchSize - 1, latestBlock);
+
+    // Query events in the current batch
+    const events = await myContract.queryFilter(
+      "BatchCreated",
+      currentBlock,
+      endBlock
+    );
+    for (let event of events) {
+      const decodedData = await decodeTransactionInput(
+        event.transactionHash,
+        provider
+      );
+      if (decodedData) {
+        decodedDataArray.push(decodedData);
+      }
+    }
+
+    // Log completion of the current batch
+    console.log(`Completed querying blocks ${currentBlock} to ${endBlock}`);
+
+    // Wait for a specified delay before the next batch
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+
+  // Save the decoded data array to a JSON file
+  try {
+    await fs.writeFile(
+      "scripts/migration/decodedData.json",
+      JSON.stringify(decodedDataArray, replacer, 2)
+    );
+    console.log("Successfully wrote decoded data to decodedData.json");
+  } catch (err) {
+    console.error("Error writing file:", err);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
