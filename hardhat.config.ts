6113ffa98ffd4f0e4fe093a320f2b4f94016de79
Marko <adriadrop@gmail.com>
2024-03-19 12:57:22 +0100

    add other addy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -135,7 +135,7 @@ const config: HardhatUserConfig = {
           privateKey: '0x9d715c14789abdc4c97fd775cf620196bebe991c60c614ba00fedbac943a5e67',
           balance: '10000000000000000000000',
         },
-        // other_1
+        // other_1 0x626178434A88c3c8809D136d500b9707D749EA9B
         {
           privateKey: 'f09baf4a06da707abeb96568a1419b4eec094774eaa85ef85517457ffe25b515',
           balance: '10000000000000000000000',
