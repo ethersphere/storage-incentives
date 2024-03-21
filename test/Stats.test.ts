611c00b762b1b58393324abdd8c9636c5f390da5
Marko <adriadrop@gmail.com>
2024-03-21 12:09:02 +0100

    change to 100 trials

--- test/Stats.test.ts
+++ test/Stats.test.ts
@@ -148,7 +148,7 @@ describe('Stats', async function () {
   });
 
   describe('two player game', async function () {
-    const trials = 150;
+    const trials = 100;
 
     it('is fair with 1:3 stake', async function () {
       this.timeout(40000);
