16d83fe92e57bdc5555069e6c0755b5e7b02c1fe
Иван Vandot <vandot@ethswarm.org>
2024-04-13 20:02:51 +0200

    fix: listen on 0.0.0.0 in every env (#250)

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,4 +4,4 @@ set -x
 
 cd ./s3 && while ! curl -m 1 -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\",\"method\":\"net_version\",\"params\":[],\"id\":67}" http://localhost:8545; do sleep 1; done && npx hardhat deploy --network localhost &
 
-yarn hardhat node
+yarn hardhat node --hostname 0.0.0.0
