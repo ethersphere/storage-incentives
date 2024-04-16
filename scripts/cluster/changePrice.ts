2c6791c254b088d527c1845482c7bfb7f50444bf
Marko <adriadrop@gmail.com>
2024-04-16 15:27:16 +0200

    add changes to comments

--- scripts/cluster/changePrice.ts
+++ scripts/cluster/changePrice.ts
@@ -6,20 +6,26 @@ function delay(milliseconds: number): Promise<void> {
 }
 
 async function main() {
-  // CLUSTER contract addresses
+  // Contracts currently deployed on CLUSTER
   const currentPostage = '0x657241f4494A2F15Ba75346E691d753A978C72Df';
   const currentOracle = '0x538E6dE1D876BBCD5667085257bc92F7c808A0F3';
   const currentStaking = '0xfc28330f1ecE0ef2371B724E0D19c1EE60B728b2';
   const currentRedis = '0x09Ad42a7d020244920309FfA14EA376dd2D3b7d5';
 
-  // Change current oracle price
+  // Change oracle price example
+
+  // Get Oracle contract at deployed address
   const oracle = await ethers.getContractAt('PriceOracle', currentOracle);
 
+  // Check current price with read only function
   const curentPrice = await oracle.currentPrice();
   console.log('Curent oracle price ', curentPrice);
+
+  // Change current price with write function
   await oracle.setPrice(24021);
   await delay(5000);
 
+  // Check value of current price
   const newPrice = await oracle.currentPrice();
   console.log('Changed oracle price ', newPrice);
 }
