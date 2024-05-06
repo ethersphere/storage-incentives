7de463ec36a6a3ed97775a31f22b6a95aba01188
Marko <adriadrop@gmail.com>
2024-05-06 10:03:31 +0200

    add testnet light

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -163,7 +163,7 @@ const config: HardhatUserConfig = {
       chainId: 12345,
       deploy: ['deploy/local/'],
     },
-    pretestnet: {
+    testnetlight: {
       url: PRIVATE_RPC_TESTNET ? PRIVATE_RPC_TESTNET : 'https://1rpc.io/sepolia',
       accounts,
       chainId: 11155111,
@@ -193,7 +193,7 @@ const config: HardhatUserConfig = {
       mainnet: mainnetEtherscanKey || '',
       sepolia: mainnetEtherscanKey || '',
       testnet: testnetEtherscanKey || '',
-      pretestnet: testnetEtherscanKey || '',
+      testnetlight: testnetEtherscanKey || '',
     },
     customChains: [
       {
@@ -205,7 +205,7 @@ const config: HardhatUserConfig = {
         },
       },
       {
-        network: 'pretestnet',
+        network: 'testnetlight',
         chainId: 11155111,
         urls: {
           apiURL: 'https://api-sepolia.etherscan.io/api',
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -11,7 +11,7 @@ export const networkConfig: networkConfigInfo = {
   localhost: { swarmNetworkId: 0, multisig: '0x62cab2b3b55f341f10348720ca18063cdb779ad5' },
   hardhat: { swarmNetworkId: 0, multisig: '0x62cab2b3b55f341f10348720ca18063cdb779ad5' },
   localcluster: { swarmNetworkId: 0, multisig: '0x62cab2b3b55f341f10348720ca18063cdb779ad5' },
-  pretestnet: {
+  testnetlight: {
     blockConfirmations: 6,
     swarmNetworkId: 333,
     multisig: '0xb1C7F17Ed88189Abf269Bf68A3B2Ed83C5276aAe',
